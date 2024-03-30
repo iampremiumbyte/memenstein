2dbf382b1c0a7119f09c7590c984b92d3a27159d
Godwin Idemudia <godwinide@gmail.com>
2024-03-30 14:09:46 +0100

    new

--- assets/js/page2.js
+++ assets/js/page2.js
@@ -1,7 +1,18 @@
 const bookwrap = document.querySelector("#bookwrap");
 const book = document.querySelector("#book");
 
+const hideMarkers = () => {
+    const markers = document.querySelectorAll(".box");
+    markers.forEach(b => b.style.display = "none !important");
+}
+
+const showMarkers = () => {
+    const markers = document.querySelectorAll(".box");
+    markers.forEach(b => b.style.display = "none !important");
+}
+
 function showbook() {
+    hideMarkers();
     bookwrap.classList.remove("no-display");
     setTimeout(() => {
         book.classList.add("animate");
@@ -9,6 +20,7 @@ function showbook() {
 }
 
 bookwrap.addEventListener("click", () => {
+    showMarkers();
     book.classList.remove("animate");
     bookwrap.classList.add("no-display");
 })
